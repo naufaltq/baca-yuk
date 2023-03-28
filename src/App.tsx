1c84d23ec240a30e85b4bc9939ce4493057bf56a
Naufaltq <naufaltq16@gmail.com>
2023-03-28 22:33:31 +0700

    Added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import AppRouter from './router';
 
 const App = () => {
   return (
     <div>
-      <h1>HELLO WORLD</h1>
+      <AppRouter/>
     </div>
   );
 }
---
+++ src/router/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppRouter = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default AppRouter;
