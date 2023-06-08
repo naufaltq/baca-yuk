f257bc06ba258c02acf49874f64d292918f1f72f
Naufaltq <naufaltq16@gmail.com>
2023-06-08 11:23:19 +0700

    update router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,37 +1,43 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from '../components/navbar';
-import NotFound404 from '../pages/404';
-import Games from '../pages/games';
-import Home from '../pages/home';
-import Learn from '../pages/learn';
-import Quiz from '../pages/quiz';
-import Read from '../pages/read';
-import Watch from '../pages/watch';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "../components/navbar";
+import Home from "../pages/home/index";
+import Read from "../pages/read";
+import Learn from "../pages/learn";
+import Quiz from "../pages/quiz";
+import Watch from "../pages/watch";
+import Adventure from "../pages/read/adventure";
+import Nature from "../pages/read/nature";
+import BookContent from "../components/books/content";
+import NotFound404 from "../pages/404";
 
 const AppRouter = () => {
-  return (
-    <Router>
-      <Navbar/>
-      <Routes>
-        <Route path='/' >
-          <Route index element={<Home/>} />
-          <Route path='read'>
-            <Route index element={<Read/>} />
-          </Route>
-          <Route path='learn' >
-            <Route index element={<Learn/>} />
-          </Route>
-          <Route path='watch' >
-            <Route index element={<Watch/>} />
-          </Route>
-          <Route path='quiz' element={<Quiz/>} />
-          <Route path='games' element={<Games/>} />
-          <Route path='*' element={<NotFound404/>} />
-        </Route>
-      </Routes>
-    </Router>
-  )
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/">
+                    <Route index element={<Home />} />
+                    <Route path="read">
+                        <Route index element={<Read />} />
+                        <Route path="adventure">
+                            <Route index element={<Adventure />} />
+                            <Route path=":id" element={<BookContent />} />
+                        </Route>
+                        <Route path="nature">
+                            <Route index element={<Nature />} />
+                            <Route path=":id" element={<BookContent />} />
+                        </Route>
+                    </Route>
+                    <Route path="learn">
+                        <Route index element={<Learn />} />
+                    </Route>
+                    <Route path="watch" element={<Watch />} />
+                    <Route path="quiz" element={<Quiz />} />
+                    <Route path="*" element={<NotFound404/>}></Route>
+                </Route>
+            </Routes>
+        </Router>
+    )
 }
 
 export default AppRouter;
