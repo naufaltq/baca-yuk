aa36e5ad9174fc5ed9f7cc4b5ac08fb3e5d84050
Naufaltq <naufaltq16@gmail.com>
2023-06-11 13:58:54 +0700

    updated the data path to redux

--- src/pages/read/nature/index.tsx
+++ src/pages/read/nature/index.tsx
@@ -1,9 +1,11 @@
 import { Container, Center } from "@chakra-ui/react";
 import { BreadcrumbForNaturePage } from "../../../components/breadcrumb";
 import NatureBooksComponent from "../../../components/nature";
+import { useAppSelector } from "../../../redux/hooks";
+import { RootState } from "../../../redux/store";
 
 const Nature = () => {
-    const natureBooksJSONData = ['/data/book/N001.json', '/data/book/N002.json', '/data/book/N003.json'];
+    const natureBooksJSONData = useAppSelector((state : RootState) => state.natureBooks.jsonData.json);
     return (
         <Container maxW='6xl' mb={10}>
             <Center>
