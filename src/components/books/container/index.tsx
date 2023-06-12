786aaab953af100302227039e683b219b7c7d5e6
Naufaltq <naufaltq16@gmail.com>
2023-06-12 19:59:54 +0700

    justify the display of book content

--- src/components/books/container/index.tsx
+++ src/components/books/container/index.tsx
@@ -8,13 +8,15 @@ const BookContainer = ({content, title}: BookContainerProps) => {
     return (
         <>
             <Center>
-                <Image src={content[currentPage].imageURL} rounded='xl' w={['80%', '100%']} h={['80%', '100%']} alt={title + '' + currentPage.toString()}></Image>
+                <Image src={content[currentPage].imageURL} rounded='xl' w={['100%', '50%']} h={['100%', '100%']} alt={title + '' + currentPage.toString()}></Image>
             </Center>
-            <Center><Text w='100%' mt={6}>{content[currentPage].description}</Text></Center>
+            <Center>
+                <Text w='100%' mt={6}>{content[currentPage].description}</Text>
+            </Center>
             <Center mt={6}>
-                {currentPage > 0 && <IconButton onClick={() => setCurrentPage(prevPage => prevPage - 1)} aria-label='Next page' icon={<FaAngleLeft />} rounded='full' />}
+                {currentPage > 0 && <IconButton onClick={() => setCurrentPage(prevPage => prevPage - 1)} aria-label='Previous page' icon={<FaAngleLeft />} rounded='full' />}
                 <Text ms={4} me={4}>{currentPage + 1} dari {content.length}</Text>
-                {currentPage + 1 < content.length && <IconButton onClick={() => setCurrentPage(prevPage => prevPage + 1)} aria-label='Next page' icon={<FaAngleRight />} rounded='full'/>}
+                {currentPage + 1 < content.length && <IconButton onClick={() => setCurrentPage(prevPage => prevPage + 1)} aria-label='Next page' icon={<FaAngleRight />} rounded='full' />}
             </Center>
         </>
   );
