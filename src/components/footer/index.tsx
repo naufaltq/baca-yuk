5cb0bdabf5d6456a179e944152713cc48952e689
Naufaltq <naufaltq16@gmail.com>
2023-07-31 19:49:50 +0700

    fix footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -68,7 +68,6 @@ import {
             <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
               <img alt="Creative Commons License" style={{ borderWidth: 0 }} src="https://i.creativecommons.org/l/by/4.0/88x31.png" />
             </a>
-            <br />
             This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
           </div>
           <Stack direction={'row'} spacing={6}>
