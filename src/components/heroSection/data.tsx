0836d7349b38110698cb7aa595e8fcbff6f590c2
Naufaltq <naufaltq16@gmail.com>
2023-08-07 20:47:38 +0700

    fix home page

---
+++ src/components/heroSection/data.tsx
@@ -0,0 +1,61 @@
+export const homeObjOne = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: "",
+    headline: "Baca Yuk",
+    description:
+      "Temukan buku cerita menarik di situs kami! Selamat datang di platform yang mengajak anda untuk membaca lebih banyak. Mari bersama-sama menjelajahi kisah-kisah menarik, pengetahuan yang memperluas wawasan, dan hiburan yang tak terhingga. Bacalah, nikmati, dan teruslah penasaran di dunia membaca ini. Yuk, membaca",
+    imgStart: "",
+    img:"logoBacaYuk.svg",
+    alt: "",
+  };
+  
+  export const homeObjTwo = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: false,
+    topLine: "Reading",
+    headline: "",
+    description: "Membaca bukan hanya aktivitas, tapi sebuah pintu menuju imajinasi yang tak terbatas dan pengetahuan yang mendalam. Di sini, anda akan menemukan beberapa ragam kategori, kisah inspiratif, dan peluang untuk meresapi kata-kata dengan segala maknanya. Mari merajut mimpi dan pengetahuan melalui keajaiban membaca. Temukan dunia baru melalui setiap halaman yang anda telusuri.",
+    imgStart: "start",
+    img: "images/baca.png",
+    alt: "read",
+  };
+  
+  export const homeObjThree = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: "Learning",
+    headline: "",
+    description: "Kami hadir untuk membantu anda memperluas dan memperkaya kosakata anda dengan cara yang interaktif dan menarik melalui sebuah gambar.",
+    imgStart: "",
+    img: "images/vocabulary.png",
+    alt: "learn",
+  };
+  
+  export const homeObjFour = {
+    lightBg: false,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: "Watching",
+    headline: " ",
+    description: "Di sini, anda dapat menemukan koleksi video cerita yang akan membawa anda dalam perjalanan tanpa batas. Dari cerita-cerita inspiratif hingga petualangan yang mengasyikkan. Jadilah penonton yang terhanyut dalam setiap adegan, dan biarkan diri anda merasakan kisah-kisah yang menginspirasi.",
+    imgStart: "start",
+    img: "images/menonton.png",
+    alt: "watch",
+  };
+  
+  export const homeObjFive = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: "Quiz",
+    headline: " ",
+    description: "Di sini, kami mengundang anda untuk menguji wawasan dan pemahaman anda melalui kuis yang menarik, kami menyajikan kuis-kuis yang tidak hanya menghibur, tetapi juga memicu keingintahuan.",
+    imgStart: "",
+    img: "images/kuis.png",
+    alt: "quiz",
+  };
+  
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,16 @@
 import Carousel from "../../components/carousel";
+import HeroSection from "../../components/heroSection";
+import { homeObjFive, homeObjFour, homeObjOne, homeObjThree, homeObjTwo } from "../../components/heroSection/data";
 
 const Home = () => {
   return (
     <div>
       <Carousel/>
+      <HeroSection {...homeObjOne}/>
+      <HeroSection {...homeObjTwo}/>
+      <HeroSection {...homeObjThree}/>
+      <HeroSection {...homeObjFour}/>
+      <HeroSection {...homeObjFive}/>
     </div>
   )
 }
